cbaf98eb499fd1df46ff74b04e34a9cd09bc8fb0
Jonathan Ramirez <jonathanramirez4479@gmail.com>
2024-06-18 22:24:24 -0700

    stage 6 state managment

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,32 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there Jonathan</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hi there Jonathan</Text>
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go to Components Demo"
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")}
+      />
+      <Button
+        title="Go to Image Screen"
+        onPress={() => navigation.navigate("Image")}
+      />
+      <Button
+        title="Go to Counter Demo"
+        onPress={() => navigation.navigate("Counter")}
+      />
+      <Button
+        title="Go to Color Demo"
+        onPress={() => navigation.navigate("Color")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,22 +1,55 @@
 import React from "react";
-import { View, Text, StyleSheet, ListScreen } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend #1" },
-    { name: "Friend #2" },
-    { name: "Friend #3" },
-    { name: "Friend #4" },
-    { name: "Friend #5" },
-    { name: "Friend #6" },
-    { name: "Friend #7" },
-    { name: "Friend #8" },
-    { name: "Friend #9" },
+    {
+      name: "Friend #1",
+      age: 20,
+    },
+    {
+      name: "Friend #2",
+      age: 45,
+    },
+    {
+      name: "Friend #3",
+      age: 32,
+    },
+    {
+      name: "Friend #4",
+      age: 27,
+    },
+    {
+      name: "Friend #5",
+      age: 53,
+    },
+    {
+      name: "Friend #7",
+      age: 30,
+    },
   ];
-  return <FlatList data={friends} renderItem={(element) => {}} />;
+
+  return (
+    <FlatList
+      showsVerticalScrollIndicator={false}
+      keyExtractor={(friend) => friend.name}
+      data={friends}
+      renderItem={({ item }) => {
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age {item.age}
+          </Text>
+        );
+      }}
+    />
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 60,
+    fontSize: 40,
+  },
+});
 
 export default ListScreen;
