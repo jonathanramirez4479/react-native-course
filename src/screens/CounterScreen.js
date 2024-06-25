d02914221f378313292d607457344e8844bfc9e5
Jonathan Ramirez <jonathanramirez@Jonathans-MacBook-Pro.local>
2024-06-24 17:39:13 -0700

    working with text input

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,24 +1,38 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+const reducer = (state, action) => {
+  // state === { counter: number }
+  // action === { type: 'increase' || 'decrease', payload: 1 }
+
+  switch (action.type) {
+    case "increase":
+      return { ...state, counter: state.counter + action.payload };
+    case "decrease":
+      return { ...state, counter: state.counter - action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
 
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          setCounter(counter + 1);
+          dispatch({ type: "increase", payload: 1 });
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          setCounter(counter - 1);
+          dispatch({ type: "decrease", payload: 1 });
         }}
       />
-      <Text>Current Count: {counter}</Text>
+      <Text>Current Count: {state.counter}</Text>
     </View>
   );
 };
